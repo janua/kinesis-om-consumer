9c3282b320b545e12dfda7153183da477eb28521
Francis Carr <fran@fcarr.com>
2014-10-19 22:11:26 +0100

    Remove license from project.clj

--- jsapp/project.clj
+++ jsapp/project.clj
@@ -1,8 +1,6 @@
 (defproject kinesis-om-template "0.0.1-SNAPSHOT"
   :description "Kinesis OM Scala/Clojurescript Template"
   :url "http://www.github.com/janua"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2342"]
                  [om "0.7.3"]]
